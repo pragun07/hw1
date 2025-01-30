d8ca8d90b4ee22c5a3816f75fcc450422a51e209
Pragun Gupta <pragungu@usc.edu>
2025-01-30 08:05:29 +0000

    all done with part 1

--- split.cpp
+++ split.cpp
@@ -18,6 +18,29 @@ void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+
+//base case when input list is empty 
+if (in==nullptr)
+{
+  return;
+}
+
+Node* temp = in;
+in = in->next;
+
+//conditional to check if value is even/odd
+if (temp -> value%2 == 0) //if value is even
+{
+  temp ->next = evens;
+  evens=temp;
+  split(in,odds,evens);
+}
+else //if value is odd
+{
+  temp ->next = odds;
+  odds=temp;
+  split(in,odds,evens);
+}
 }
 
 /* If you needed a helper function, write it here */
